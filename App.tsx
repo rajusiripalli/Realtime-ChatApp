06a5a5f77e81a46651364175807cf0ba6ca9abd1
rajusiripalli <rajusiripalli7@gmail.com>
2025-10-20 08:21:11 +0530

    project setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1  items-center justify-center ">
+      <Text className="text-3xl font-bold text-blue-500">Jesus Loves You!</Text>
       <StatusBar style="auto" />
     </View>
   );
