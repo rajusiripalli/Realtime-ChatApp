3dae3668771c5176bff3884bf4a801cb7b0c8c53
rajusiripalli <rajusiripalli7@gmail.com>
2025-11-14 12:24:59 +0530

    channel users supabase tables users 1hr 45 mins completed

--- src/app/(drawer)/(home)/new/chat.tsx
+++ src/app/(drawer)/(home)/new/chat.tsx
@@ -1,8 +1,14 @@
 import UserList from '@/components/UserList';
+import { useSupabase } from '@/providers/SupabaseProvider';
 import { User } from '@/types';
+import { useUser } from '@clerk/clerk-expo';
+import { useMutation } from '@tanstack/react-query';
 import { View } from 'react-native';
 
 export default function NewChat() {
+  const supabase = useSupabase();
+  const {user} = useUser();
+  
   const handleUserPress = (user: User) => {
     console.log('User clicked: ', user.first_name);
   };
